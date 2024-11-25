c427402a5fd09cd3b3d0eee18fe0c20aa18738d6
MEliwas <dimapogodin742@gmail.com>
2024-11-25 20:40:02 +0300

    settings

---
+++ settings/settings.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Menu/menu.tscn")
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0,value/5)
+
+
+func _on_mute_volume_toggled(toggled_on):
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_resolution_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
+		3:
+			DisplayServer.window_set_size(Vector2i(1152,648))
